286a6844692cb2ac4893e5168e54db49acd25f93
zi <salicrim@indracompany.com>
2021-01-24 12:22:04 -0300

    inital setup peoject and data

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,8 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+  
 ]
 
 const router = createRouter({
@@ -23,3 +17,5 @@ const router = createRouter({
 })
 
 export default router
+
+
